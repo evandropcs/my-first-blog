e0d3896d92c638fe4ab7458f54bf9af4c39b07ab
evandropcs <evandropcs@hotmail.com>
2022-07-19 00:15:31 -0300

    modifica templates e os mostra os posts do db

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Post
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('title')
+    # contex={
+    #     'posts': posts
+    # }
+    return render(request, 'blog/post_list.html', {'posts': posts})

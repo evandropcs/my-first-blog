967bd928e2f0c530e8c452370725887f3def9d10
evandropcs <evandropcs@hotmail.com>
2022-07-19 20:01:15 -0300

    seta debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o5q(7owp0)g0!wrv)@rxyw9(@6&v8&xcpnpro+-9%#x2%h20_j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
